e53f07d45273b4dbf974b8a4736f46a047fc018d
Sam-Code0607 <franciscocode.dev@gmail.com>
2026-05-16 08:20:39 -0500

    feat: EventGenerator added with police checkout

--- src/main/java/com/ds/core/simulation/EventGenerator.java
+++ src/main/java/com/ds/core/simulation/EventGenerator.java
@@ -1,16 +1,17 @@
 package com.ds.core.simulation;
 
-
-import com.ds.core.graph.Graph;
-import com.ds.core.models.Edge;
-
 import java.util.List;
 import java.util.Random;
 
-/**
- * Genera eventos aleatorios dentro del mapa.
- * Estos eventos afectan temporalmente las rutas del grafo.
- */
+import com.ds.core.enums.RouteStatus;
+import com.ds.core.graph.Graph;
+import com.ds.core.models.Edge;
+
+/// Clase responsable de generar eventos aleatorios que afectan el 
+// estado de las rutas en el grafo, como tráfico, accidentes o bloqueos temporales. 
+// Estos eventos simulan condiciones dinámicas en la red de rutas, lo que permite,
+// por ejemplo, que los algoritmos de búsqueda (Dijkstra, BFS) respondan a cambios en tiempo real.
+
 public class EventGenerator {
 
     private final Graph graph;
@@ -20,68 +21,73 @@ public class EventGenerator {
         this.graph = graph;
         this.random = new Random();
     }
+    //Metodo para simular que la ruta esta bloqueada temporalmente
+    // lo que afecta la disponibilidad de rutas para los algoritmos de busqueda.
 
-    /**
-     * Genera un evento aleatorio sobre una arista random del grafo.
-     */
     public void generateRandomEvent() {
 
-        // obtener todas las conexiones del grafo
-        List<Edge> edges = graph.getEdges();
+        List<Edge> edges = graph.getAllEdges();
 
-        // validar que existan rutas
         if (edges.isEmpty()) {
             System.out.println("No hay rutas disponibles.");
             return;
         }
 
-        // seleccionar una arista aleatoria
         Edge selectedEdge = edges.get(random.nextInt(edges.size()));
 
-        // seleccionar tipo de evento aleatorio
         int eventType = random.nextInt(3);
 
         switch (eventType) {
 
-            // tráfico
+            // Metodo y caso de tráfico entre nodo A y B
             case 0:
-                selectedEdge.setWeight(selectedEdge.getWeight() + 3);
+
+                selectedEdge.setStatus(RouteStatus.CONGESTED);
 
                 System.out.println(
                         "[EVENTO] Tráfico detectado entre "
-                                + selectedEdge.getOriginId()
-                                + " -> "
-                                + selectedEdge.getDestinationId()
-                                + " | Nuevo peso: "
-                                + selectedEdge.getWeight()
+                                + selectedEdge.getFrom().getName()
+                                + " ----> "
+                                + selectedEdge.getTo().getName()
                 );
                 break;
 
-            // accidente
+            // Metodo y caso de accidente entre nodo A y B
             case 1:
-                selectedEdge.setWeight(selectedEdge.getWeight() + 7);
+
+                selectedEdge.setStatus(RouteStatus.CONGESTED);
 
                 System.out.println(
                         "[EVENTO] Accidente detectado entre "
-                                + selectedEdge.getOriginId()
-                                + " -> "
-                                + selectedEdge.getDestinationId()
-                                + " | Nuevo peso: "
-                                + selectedEdge.getWeight()
+                                + selectedEdge.getFrom().getName()
+                                + " ----> "
+                                + selectedEdge.getTo().getName()
                 );
                 break;
 
-            // bloqueo
+            // Metodo y caso de bloqueo entre nodo A y B
             case 2:
+
                 selectedEdge.setBlocked(true);
+                selectedEdge.setStatus(RouteStatus.BLOCKED);
 
                 System.out.println(
                         "[EVENTO] Ruta bloqueada entre "
-                                + selectedEdge.getOriginId()
-                                + " -> "
-                                + selectedEdge.getDestinationId()
+                                + selectedEdge.getFrom().getName()
+                                + " ----> "
+                                + selectedEdge.getTo().getName()
                 );
                 break;
+            
+            //Metodo y caso de reten policial entre nodo A y B
+            case 3: 
+            selectedEdge.setStatus(RouteStatus.CONGESTED);
+            System.out.println(
+                "[EVENTO] Retén policial entre "
+                        + selectedEdge.getFrom().getName()
+                        + " ----> "
+                        + selectedEdge.getTo().getName()
+            );
         }
     }
 }
